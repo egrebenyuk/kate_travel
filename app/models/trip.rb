7d11c7b8d32e5414790eea929c5d894d6f85acd7
Katherine Walker <katewalker171@gmail.com>
2015-02-22 17:26:03 -0500

    Add destinations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,4 @@
 class Trip < ActiveRecord::Base
+  has_many :destinations
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219193510) do
+ActiveRecord::Schema.define(version: 20150219222207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "destinations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "trip_id"
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string   "name"
